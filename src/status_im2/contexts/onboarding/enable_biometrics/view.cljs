a72c23941609f953b93f3315c177129e13cfd582
Brian Sztamfater <brian@status.im>
2023-03-13 15:47:05 -0300

    feat: enable biometrics screen

--- src/status_im2/contexts/onboarding/enable_biometrics/view.cljs
+++ src/status_im2/contexts/onboarding/enable_biometrics/view.cljs
@@ -5,7 +5,8 @@
             [status-im2.contexts.onboarding.enable-biometrics.style :as style]
             [utils.i18n :as i18n]
             [status-im2.contexts.onboarding.common.background.view :as background]
-            [utils.re-frame :as rf]))
+            [utils.re-frame :as rf]
+            [status-im.multiaccounts.biometric.core :as biometric]))
 
 (defn navigation-bar
   []
@@ -17,18 +18,34 @@
 
 (defn page
   []
-  [rn/view {:style style/page-container}
-   [navigation-bar]
-   [rn/view {:style {:padding-horizontal 20}}
-    [quo/text
-     {:size   :heading-1
-      :weight :semi-bold
-      :style  {:color colors/white}} "Enable-biometrics"]
-    [quo/button
-     {:on-press       #(rf/dispatch [:navigate-to :enable-notifications])
-      :type           :grey
-      :override-theme :dark
-      :style          {}} (i18n/label :t/continue)]]])
+  (let [supported-biometric (rf/sub [:supported-biometric-auth])
+        bio-type-label (biometric/get-label supported-biometric)]
+   [rn/view {:style style/page-container}
+    [navigation-bar]
+    [rn/view {:style {:padding-horizontal 20}}
+     [quo/text
+      {:size   :heading-1
+       :weight :semi-bold
+       :style  {:color colors/white}} (i18n/label :t/enable-biometrics)]
+     [quo/text
+      {:size   :paragraph-1
+       :style  {:color      colors/white
+                :margin-top 8}}
+      (i18n/label :t/use-biometrics)]
+     [rn/view {:style style/image-container}
+      [quo/text {:size   :paragraph-1}
+       "Illustration here"]]
+     [rn/view 
+      [quo/button
+      {:on-press       #(rf/dispatch [:navigate-to :enable-notifications])
+       :type           :grey
+       :override-theme :dark
+       :style          {}} (i18n/label :t/biometric-enable-button {:bio-type-label bio-type-label})]
+     [quo/button
+      {:on-press       #(rf/dispatch [:navigate-to :enable-notifications])
+       :type           :grey
+       :override-theme :dark
+       :style          {:margin-top 12}} (i18n/label :t/maybe-later)]]]]))
 
 (defn enable-biometrics
   []
