82b28e865a15c62808a8f021a9cb30fecfc18140
Ulises M <ulises.ssb@hotmail.com>
2023-03-17 12:51:12 -0600

    Create navigation-bar component for onboarding

---
+++ src/status_im2/contexts/onboarding/common/navigation_bar/style.cljs
@@ -0,0 +1,3 @@
+(ns status-im2.contexts.onboarding.common.navigation-bar.style)
+
+(def navigation-bar {:height 56})
---
+++ src/status_im2/contexts/onboarding/common/navigation_bar/view.cljs
@@ -0,0 +1,20 @@
+(ns status-im2.contexts.onboarding.common.navigation-bar.view
+  (:require [quo2.core :as quo]
+            [react-native.core :as rn]
+            [status-im2.contexts.onboarding.common.navigation-bar.style :as style]
+            [utils.re-frame :as rf]))
+
+(defn navigation-bar
+  [{:keys [on-press-info]}]
+  [rn/view {:style style/navigation-bar}
+   [quo/page-nav
+    {:align-mid?            true
+     :mid-section           {:type :text-only}
+     :left-section          {:type                :blur-bg
+                             :icon                :i/arrow-left
+                             :icon-override-theme :dark
+                             :on-press            #(rf/dispatch [:navigate-back])}
+     :right-section-buttons [{:type                :blur-bg
+                              :icon                :i/info
+                              :icon-override-theme :dark
+                              :on-press            on-press-info}]}]])
