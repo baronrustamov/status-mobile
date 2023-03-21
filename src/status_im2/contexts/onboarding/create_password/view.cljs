61a85778f7d4b69e56367776a3a809047078dd77
Ulises M <ulises.ssb@hotmail.com>
2023-03-21 15:24:53 -0600

    Delete ratom update on render-function's body

--- src/status_im2/contexts/onboarding/create_password/view.cljs
+++ src/status_im2/contexts/onboarding/create_password/view.cljs
@@ -54,38 +54,35 @@
 
 (defn password-inputs
   [{:keys [passwords-match? on-change-password on-change-repeat-password on-input-focus
-           password-long-enough? empty-password? same-password-length?]}]
-  (reagent/with-let [show-hint-2? (reagent/atom false)]
-    (let [_ (when same-password-length? (reset! show-hint-2? true))
-          hint-1-status (if password-long-enough? :success :neutral)
-          hint-2-status (if passwords-match? :success :danger)
-          hint-2-text   (if passwords-match?
-                          (i18n/label :t/password-creation-match)
-                          (i18n/label :t/password-creation-dont-match))
-          error?        (and @show-hint-2?
-                             (not passwords-match?)
-                             (not empty-password?))]
-      [:<>
-       [password-with-hint
-        {:hint           {:text   (i18n/label :t/password-creation-hint)
-                          :status hint-1-status
-                          :shown  true}
-         :placeholder    (i18n/label :t/password-creation-placeholder-1)
-         :on-change-text on-change-password
-         :on-focus       #(on-input-focus :password)}]
-       [rn/view {:style style/space-between-inputs}]
-       [password-with-hint
-        {:hint           {:text   hint-2-text
-                          :status hint-2-status
-                          :shown  (and (not empty-password?)
-                                       @show-hint-2?)}
-         :error?         error?
-         :placeholder    (i18n/label :t/password-creation-placeholder-2)
-         :on-change-text on-change-repeat-password
-         :on-focus       #(on-input-focus :repeat-password)
-         :on-blur        #(if empty-password?
-                            (reset! show-hint-2? false)
-                            (reset! show-hint-2? true))}]])))
+           password-long-enough? empty-password? show-password-validation?
+           on-blur-repeat-password]}]
+  (let [hint-1-status (if password-long-enough? :success :neutral)
+        hint-2-status (if passwords-match? :success :danger)
+        hint-2-text   (if passwords-match?
+                        (i18n/label :t/password-creation-match)
+                        (i18n/label :t/password-creation-dont-match))
+        error?        (and show-password-validation?
+                           (not passwords-match?)
+                           (not empty-password?))]
+    [:<>
+     [password-with-hint
+      {:hint           {:text   (i18n/label :t/password-creation-hint)
+                        :status hint-1-status
+                        :shown  true}
+       :placeholder    (i18n/label :t/password-creation-placeholder-1)
+       :on-change-text on-change-password
+       :on-focus       #(on-input-focus :password)}]
+     [rn/view {:style style/space-between-inputs}]
+     [password-with-hint
+      {:hint           {:text   hint-2-text
+                        :status hint-2-status
+                        :shown  (and (not empty-password?)
+                                     show-password-validation?)}
+       :error?         error?
+       :placeholder    (i18n/label :t/password-creation-placeholder-2)
+       :on-change-text on-change-repeat-password
+       :on-focus       #(on-input-focus :repeat-password)
+       :on-blur        on-blur-repeat-password}]]))
 
 (def strength-status
   {1 :very-weak
@@ -129,68 +126,81 @@
 
 (defn password-form
   [{:keys [scroll-to-end-fn]}]
-  (reagent/with-let [state               (reagent/atom {:password            ""
-                                                        :repeat-password     ""
-                                                        :accepts-disclaimer? false
-                                                        :focused-input       nil})
-                     password            (reagent/cursor state [:password])
-                     repeat-password     (reagent/cursor state [:repeat-password])
-                     accepts-disclaimer? (reagent/cursor state [:accepts-disclaimer?])
-                     focused-input       (reagent/cursor state [:focused-input])]
-    (let [{:keys [long-enough?]
-           :as   validations} (password-validations @password)
-          password-strength   (calc-password-strength validations)
-          empty-password?     (empty? @password)
-          same-passwords?     (= @password @repeat-password)
-          same-length?        (and (not empty-password?)
-                                   (= (count @password) (count @repeat-password)))
-          meet-requirements?  (and (not empty-password?)
-                                   (= password-strength 5)
-                                   same-passwords?
-                                   @accepts-disclaimer?)]
-      [:<>
-       [rn/view {:style style/top-part}
-        [header]
-        [password-inputs
-         {:password-long-enough?     long-enough?
-          :passwords-match?          same-passwords?
-          :empty-password?           empty-password?
-          :same-password-length?     same-length?
-          :on-input-focus            (fn [input-id]
-                                       (scroll-to-end-fn)
-                                       (reset! focused-input input-id))
-          :on-change-password        #(reset! password %)
-          :on-change-repeat-password #(reset! repeat-password %)}]]
+  (let [state                     (reagent/atom {:password                  ""
+                                                 :repeat-password           ""
+                                                 :accepts-disclaimer?       false
+                                                 :focused-input             nil
+                                                 :show-password-validation? false})
+        password                  (reagent/cursor state [:password])
+        repeat-password           (reagent/cursor state [:repeat-password])
+        accepts-disclaimer?       (reagent/cursor state [:accepts-disclaimer?])
+        focused-input             (reagent/cursor state [:focused-input])
+        show-password-validation? (reagent/atom false)
+        same-password-length?     #(and (seq @password)
+                                        (= (count @password) (count @repeat-password)))]
+    (fn []
+      (let [{:keys [long-enough?]
+             :as   validations} (password-validations @password)
+            password-strength   (calc-password-strength validations)
+            empty-password?     (empty? @password)
+            same-passwords?     (= @password @repeat-password)
+            meet-requirements?  (and (not empty-password?)
+                                     (= password-strength 5)
+                                     same-passwords?
+                                     @accepts-disclaimer?)]
+        [:<>
+         [rn/view {:style style/top-part}
+          [header]
+          [password-inputs
+           {:password-long-enough?     long-enough?
+            :passwords-match?          same-passwords?
+            :empty-password?           empty-password?
+            :show-password-validation? @show-password-validation?
+            :on-input-focus            (fn [input-id]
+                                         (scroll-to-end-fn)
+                                         (reset! focused-input input-id))
+            :on-change-password        (fn [new-value]
+                                         (reset! password new-value)
+                                         (when (same-password-length?)
+                                           (reset! show-password-validation? true)))
+            :on-change-repeat-password (fn [new-value]
+                                         (reset! repeat-password new-value)
+                                         (when (same-password-length?)
+                                           (reset! show-password-validation? true)))
+            :on-blur-repeat-password   #(if empty-password?
+                                          (reset! show-password-validation? false)
+                                          (reset! show-password-validation? true))}]]
 
-       [rn/view {:style style/bottom-part}
-        (when (= @focused-input :password)
-          [help
-           {:validations       validations
-            :password-strength password-strength}])
+         [rn/view {:style style/bottom-part}
+          (when (= @focused-input :password)
+            [help
+             {:validations       validations
+              :password-strength password-strength}])
 
-        (when (= @focused-input :repeat-password)
-          [rn/view {:style style/disclaimer-container}
-           [quo/disclaimer
-            {:on-change #(reset! accepts-disclaimer? %)
-             :checked?  @accepts-disclaimer?}
-            (i18n/label :t/password-creation-disclaimer)]])
+          (when (= @focused-input :repeat-password)
+            [rn/view {:style style/disclaimer-container}
+             [quo/disclaimer
+              {:on-change #(reset! accepts-disclaimer? %)
+               :checked?  @accepts-disclaimer?}
+              (i18n/label :t/password-creation-disclaimer)]])
 
-        [rn/view {:style style/button-container}
-         [quo/button
-          {:disabled (not meet-requirements?)
-           :on-press #(rf/dispatch [:navigate-to :enable-biometrics])}
-          (i18n/label :t/password-creation-confirm)]]]])))
+          [rn/view {:style style/button-container}
+           [quo/button
+            {:disabled (not meet-requirements?)
+             :on-press #(rf/dispatch [:navigate-to :enable-biometrics])}
+            (i18n/label :t/password-creation-confirm)]]]]))))
 
 (defn create-password
   []
-  (reagent/with-let [scroll-view-ref  (atom nil)
-                     scroll-to-end-fn #(js/setTimeout (.-scrollToEnd @scroll-view-ref)
-                                                      250)]
-    [rn/view
-     [background-image]
-     [rn/scroll-view
-      {:ref                     #(reset! scroll-view-ref %)
-       :style                   style/overlay
-       :content-container-style style/content-style}
-      [navigation-bar/navigation-bar {:on-press-info #(js/alert "Info pressed")}]
-      [password-form {:scroll-to-end-fn scroll-to-end-fn}]]]))
+  (let [scroll-view-ref  (atom nil)
+        scroll-to-end-fn #(js/setTimeout (.-scrollToEnd @scroll-view-ref) 250)]
+    (fn []
+      [rn/view
+       [background-image]
+       [rn/scroll-view
+        {:ref                          #(reset! scroll-view-ref %)
+         :style                        style/overlay
+         :content-container-style      style/content-style
+         :keyboard-should-persist-taps true}
+        [navigation-bar/navigation-bar {:on-press-info #(js/alert "Info pressed")}]
+        [password-form {:scroll-to-end-fn scroll-to-end-fn}]]])))
