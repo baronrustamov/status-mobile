c10578d3e95aa937c926f27b08dd3262d2f1874e
alwx <alwxndr@gmail.com>
2023-03-21 12:32:25 +0100

    Fix for sender's name, profile icon, chat key, timestamp being lost for messages that contain images

--- src/status_im2/subs/chat/messages.cljs
+++ src/status_im2/subs/chat/messages.cljs
@@ -19,17 +19,17 @@
   so we bucket both in 1999-12-31"
   [{:keys [acc last-timestamp last-datemark]} {:keys [whisper-timestamp datemark] :as msg}]
   (cond
-    (empty? acc) ; initial element
+    (empty? acc)                                            ; initial element
     {:last-timestamp whisper-timestamp
      :last-datemark  datemark
      :acc            (conj acc msg)}
 
-    (and (not= last-datemark datemark) ; not the same day
-         (< whisper-timestamp last-timestamp)) ; not out-of-order
+    (and (not= last-datemark datemark)                      ; not the same day
+         (< whisper-timestamp last-timestamp))              ; not out-of-order
     {:last-timestamp whisper-timestamp
      :last-datemark  datemark
      :acc            (conj acc
-                           {:value last-datemark ; intersperse datemark message
+                           {:value last-datemark            ; intersperse datemark message
                             :type  :datemark}
                            msg)}
     :else
@@ -64,35 +64,35 @@
   It will also append one last gap if the last message is a non-gap"
   [messages chat-id synced-from now chat-type joined loading-messages?]
   (let [messages-with-gaps (reduce
-                            (fn [acc {:keys [gap-parameters message-id] :as message}]
-                              (let [last-element (peek acc)]
-                                (cond
-                                  ;; If it's a message, just add
-                                  (empty? gap-parameters)
-                                  (conj acc message)
+                             (fn [acc {:keys [gap-parameters message-id] :as message}]
+                               (let [last-element (peek acc)]
+                                 (cond
+                                   ;; If it's a message, just add
+                                   (empty? gap-parameters)
+                                   (conj acc message)
 
-                                  ;; Both are gaps, merge them
-                                  (and
-                                   (seq (:gap-parameters last-element))
-                                   (seq gap-parameters))
-                                  (conj (pop acc) (update last-element :gap-ids conj message-id))
+                                   ;; Both are gaps, merge them
+                                   (and
+                                     (seq (:gap-parameters last-element))
+                                     (seq gap-parameters))
+                                   (conj (pop acc) (update last-element :gap-ids conj message-id))
 
-                                  ;; it's a gap
-                                  :else
-                                  (conj acc (assoc message :gap-ids #{message-id})))))
-                            []
-                            messages)]
-    (if (or loading-messages? ; it's loading messages from the database
-            (nil? synced-from) ; it's still syncing
-            (= constants/timeline-chat-type chat-type) ; it's a timeline chat
-            (= constants/profile-chat-type chat-type) ; it's a profile chat
-            (and (not (nil? synced-from)) ; it's not more than a month
+                                   ;; it's a gap
+                                   :else
+                                   (conj acc (assoc message :gap-ids #{message-id})))))
+                             []
+                             messages)]
+    (if (or loading-messages?                               ; it's loading messages from the database
+            (nil? synced-from)                              ; it's still syncing
+            (= constants/timeline-chat-type chat-type)      ; it's a timeline chat
+            (= constants/profile-chat-type chat-type)       ; it's a profile chat
+            (and (not (nil? synced-from))                   ; it's not more than a month
                  (<= synced-from (- (quot now 1000) constants/one-month)))
             (and (= constants/private-group-chat-type chat-type) ; it's a private group chat
-                 (or (not (pos? joined)) ; we haven't joined
-                     (>= (quot joined 1000) synced-from))) ; the history goes before we joined
-            (:gap-ids (peek messages-with-gaps))) ; there's already a gap on top of the chat history
-      messages-with-gaps ; don't add an extra gap
+                 (or (not (pos? joined))                    ; we haven't joined
+                     (>= (quot joined 1000) synced-from)))  ; the history goes before we joined
+            (:gap-ids (peek messages-with-gaps)))           ; there's already a gap on top of the chat history
+      messages-with-gaps                                    ; don't add an extra gap
       (conj messages-with-gaps (last-gap chat-id synced-from)))))
 
 (defn hydrate-messages
@@ -102,63 +102,58 @@
    (keep #(if (= :message (% :type))
             (when-let [message (messages (% :message-id))]
               (let [pinned-message (get pinned-messages (% :message-id))
-                    pinned         (if pinned-message true (some? (message :pinned-by)))
-                    pinned-by      (when pinned (or (message :pinned-by) (pinned-message :pinned-by)))
-                    message        (assoc message :pinned pinned :pinned-by pinned-by)]
+                    pinned (if pinned-message true (some? (message :pinned-by)))
+                    pinned-by (when pinned (or (message :pinned-by) (pinned-message :pinned-by)))
+                    message (assoc message :pinned pinned :pinned-by pinned-by)]
                 (merge message %)))
             %)
          message-list)))
 
 (defn albumize-messages
   [messages]
-  (get
-   (reduce (fn [{:keys [messages albums]} message]
-             (let [album-id (:album-id message)
-                   albums   (cond-> albums album-id (update album-id conj message))
-                   messages (if album-id
-                              (conj (filterv #(not= album-id (:album-id %)) messages)
-                                    {:album           (get albums album-id)
-                                     :album-id        album-id
-                                     :albumize?       (:albumize? message)
-                                     :message-id      (:message-id message)
-                                     :deleted?        (:deleted? message)
-                                     :deleted-for-me? (:deleted-for-me? message)
-                                     :deleted-by      (:deleted-by message)
-                                     :from            (:from message)
-                                     :timestamp-str   (:timestamp-str message)
-                                     :content-type    constants/content-type-album})
-                              (conj messages message))]
-               {:messages messages
-                :albums   albums}))
-           {:messages []
-            :albums   {}}
-           messages)
-   :messages))
+  (->> messages
+       (reduce (fn [{:keys [messages albums]} message]
+                 (let [album-id (:album-id message)
+                       albums (cond-> albums
+                                      album-id
+                                      (update album-id conj message))
+                       messages (if album-id
+                                  (conj (filterv #(not= album-id (:album-id %)) messages)
+                                        (merge message
+                                               {:album        (get albums album-id)
+                                                :album-id     album-id
+                                                :content-type constants/content-type-album}))
+                                  (conj messages message))]
+                   {:messages messages
+                    :albums   albums}))
+               {:messages []
+                :albums   {}})
+       :messages))
 
 (re-frame/reg-sub
- :chats/chat-messages
- :<- [:messages/messages]
- (fn [messages [_ chat-id]]
-   (get messages chat-id {})))
+  :chats/chat-messages
+  :<- [:messages/messages]
+  (fn [messages [_ chat-id]]
+    (get messages chat-id {})))
 
 (re-frame/reg-sub
- :chats/pinned
- (fn [[_ chat-id] _]
-   [(re-frame/subscribe [:messages/pin-messages])
-    (re-frame/subscribe [:chats/chat-messages chat-id])])
- (fn [[pin-messages messages] [_ chat-id]]
-   (let [pinned-messages (get pin-messages chat-id {})]
-     (reduce-kv
-      (fn [pinned-messages pinned-message-id pinned-message]
-        (let [{:keys [deleted? deleted-for-me? deleted-by]} (get messages pinned-message-id)
-              pinned-message                                (assoc pinned-message
-                                                                   :deleted?        deleted?
-                                                                   :deleted-for-me? deleted-for-me?
-                                                                   :deleted-by      deleted-by
-                                                                   :pinned          true)]
-          (assoc pinned-messages pinned-message-id pinned-message)))
-      pinned-messages
-      pinned-messages))))
+  :chats/pinned
+  (fn [[_ chat-id] _]
+    [(re-frame/subscribe [:messages/pin-messages])
+     (re-frame/subscribe [:chats/chat-messages chat-id])])
+  (fn [[pin-messages messages] [_ chat-id]]
+    (let [pinned-messages (get pin-messages chat-id {})]
+      (reduce-kv
+        (fn [pinned-messages pinned-message-id pinned-message]
+          (let [{:keys [deleted? deleted-for-me? deleted-by]} (get messages pinned-message-id)
+                pinned-message (assoc pinned-message
+                                 :deleted? deleted?
+                                 :deleted-for-me? deleted-for-me?
+                                 :deleted-by deleted-by
+                                 :pinned true)]
+            (assoc pinned-messages pinned-message-id pinned-message)))
+        pinned-messages
+        pinned-messages))))
 
 ;; local messages will not have a :pinned-at key until user navigates away and to
 ;; chat screen. For this reason we want to retain order of local messages with :pinned-at nil
@@ -168,84 +163,84 @@
   [a b]
   (cond
     (and a b) (- a b)
-    (or a b)  (if b false true)
-    :else     a))
+    (or a b) (if b false true)
+    :else a))
 
 (re-frame/reg-sub
- :chats/pinned-sorted-list
- (fn [[_ chat-id] _]
-   (re-frame/subscribe [:chats/pinned chat-id]))
- (fn [pin-messages _]
-   (let [pin-messages-vals (vals pin-messages)]
+  :chats/pinned-sorted-list
+  (fn [[_ chat-id] _]
+    (re-frame/subscribe [:chats/pinned chat-id]))
+  (fn [pin-messages _]
+    (let [pin-messages-vals (vals pin-messages)]
 
-     (sort-by :pinned-at sort-pinned pin-messages-vals))))
-
-(re-frame/reg-sub
- :chats/pin-modal
- :<- [:messages/pin-modal]
- (fn [pin-modal [_ chat-id]]
-   (get pin-modal chat-id)))
+      (sort-by :pinned-at sort-pinned pin-messages-vals))))
 
 (re-frame/reg-sub
- :chats/message-reactions
- :<- [:multiaccount/public-key]
- :<- [:messages/reactions]
- (fn [[current-public-key reactions] [_ message-id chat-id]]
-   (models.reactions/message-reactions
-    current-public-key
-    (get-in reactions [chat-id message-id]))))
+  :chats/pin-modal
+  :<- [:messages/pin-modal]
+  (fn [pin-modal [_ chat-id]]
+    (get pin-modal chat-id)))
 
 (re-frame/reg-sub
- :chats/all-loaded?
- :<- [:messages/pagination-info]
- (fn [pagination-info [_ chat-id]]
-   (get-in pagination-info [chat-id :all-loaded?])))
+  :chats/message-reactions
+  :<- [:multiaccount/public-key]
+  :<- [:messages/reactions]
+  (fn [[current-public-key reactions] [_ message-id chat-id]]
+    (models.reactions/message-reactions
+      current-public-key
+      (get-in reactions [chat-id message-id]))))
 
 (re-frame/reg-sub
- :chats/loading-messages?
- :<- [:messages/pagination-info]
- (fn [pagination-info [_ chat-id]]
-   (get-in pagination-info [chat-id :loading-messages?])))
+  :chats/all-loaded?
+  :<- [:messages/pagination-info]
+  (fn [pagination-info [_ chat-id]]
+    (get-in pagination-info [chat-id :all-loaded?])))
 
 (re-frame/reg-sub
- :chats/message-list
- :<- [:messages/message-lists]
- (fn [message-lists [_ chat-id]]
-   (get message-lists chat-id)))
+  :chats/loading-messages?
+  :<- [:messages/pagination-info]
+  (fn [pagination-info [_ chat-id]]
+    (get-in pagination-info [chat-id :loading-messages?])))
 
 (re-frame/reg-sub
- :chats/raw-chat-messages-stream
- (fn [[_ chat-id] _]
-   [(re-frame/subscribe [:chats/message-list chat-id])
-    (re-frame/subscribe [:chats/chat-messages chat-id])
-    (re-frame/subscribe [:chats/pinned chat-id])
-    (re-frame/subscribe [:chats/loading-messages? chat-id])
-    (re-frame/subscribe [:chats/synced-from chat-id])
-    (re-frame/subscribe [:chats/chat-type chat-id])
-    (re-frame/subscribe [:chats/joined chat-id])])
- (fn [[message-list messages pin-messages loading-messages? synced-from chat-type joined] [_ chat-id]]
-   ;;TODO (perf)
-   (let [message-list-seq (models.message-list/->seq message-list)]
-     ; Don't show gaps if that's the case as we are still loading messages
-     (if (and (empty? message-list-seq) loading-messages?)
-       []
-       (-> message-list-seq
-           (add-datemarks)
-           (hydrate-messages messages pin-messages)
-           (collapse-gaps chat-id
-                          synced-from
-                          (datetime/timestamp)
-                          chat-type
-                          joined
-                          loading-messages?)
-           (albumize-messages))))))
+  :chats/message-list
+  :<- [:messages/message-lists]
+  (fn [message-lists [_ chat-id]]
+    (get message-lists chat-id)))
 
 (re-frame/reg-sub
- :messages/resolve-mention
- (fn [[_ mention] _]
-   [(re-frame/subscribe [:contacts/contact-name-by-identity mention])])
- (fn [[contact-name] [_ mention]]
-   (if (= mention constants/everyone-mention-id)
-     (i18n/label :t/everyone-mention)
-     contact-name)))
+  :chats/raw-chat-messages-stream
+  (fn [[_ chat-id] _]
+    [(re-frame/subscribe [:chats/message-list chat-id])
+     (re-frame/subscribe [:chats/chat-messages chat-id])
+     (re-frame/subscribe [:chats/pinned chat-id])
+     (re-frame/subscribe [:chats/loading-messages? chat-id])
+     (re-frame/subscribe [:chats/synced-from chat-id])
+     (re-frame/subscribe [:chats/chat-type chat-id])
+     (re-frame/subscribe [:chats/joined chat-id])])
+  (fn [[message-list messages pin-messages loading-messages? synced-from chat-type joined] [_ chat-id]]
+    ;;TODO (perf)
+    (let [message-list-seq (models.message-list/->seq message-list)]
+      ; Don't show gaps if that's the case as we are still loading messages
+      (if (and (empty? message-list-seq) loading-messages?)
+        []
+        (-> message-list-seq
+            (add-datemarks)
+            (hydrate-messages messages pin-messages)
+            (collapse-gaps chat-id
+                           synced-from
+                           (datetime/timestamp)
+                           chat-type
+                           joined
+                           loading-messages?)
+            (albumize-messages))))))
+
+(re-frame/reg-sub
+  :messages/resolve-mention
+  (fn [[_ mention] _]
+    [(re-frame/subscribe [:contacts/contact-name-by-identity mention])])
+  (fn [[contact-name] [_ mention]]
+    (if (= mention constants/everyone-mention-id)
+      (i18n/label :t/everyone-mention)
+      contact-name)))
 
